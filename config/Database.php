ab72163c9df816054cad975d1491bb48fc939f6e
FernandoDuffoo <fduffoo@aol.com>
2023-03-18 21:57:49 -0400

    Updated Database.php for Render

--- config/Database.php
+++ config/Database.php
@@ -2,10 +2,10 @@
 
 class Database {
     // DB Params
-    private $host = 'localhost';
-    private $db_name = 'quotesdb';
-    private $username = 'root';
-    private $password = '';
+    private $host = 'dpg-cgb4uihmbg55nql1c3p0-a';
+    private $db_name = 'quotesdb_zu7u';
+    private $username = 'fduffoo';
+    private $password = 'NP8hVT92m4BGGF1CZPTaRHHOFV3BT5DR';
     private $conn;
 
     // DB Connect
