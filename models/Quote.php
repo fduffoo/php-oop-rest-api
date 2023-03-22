5cb8ad6c7ddbc50b15f9be17a5ef37f218c949a4
FernandoDuffoo <fduffoo@aol.com>
2023-03-22 12:15:12 -0400

    fix

--- models/Quote.php
+++ models/Quote.php
@@ -70,8 +70,8 @@ class Quote {
     // Set properties
     $this->id = $row ['id'];
     $this->quote = $row ['quote'];
-    $this->author_id = $row ['author_id'];     
-    $this->category_id = $row ['category_id']; 
+    $this->author = $row ['author'];     //both_id
+    $this->category = $row ['category']; //
 
     }
 
