420273e7213e636683684dc19f4a675bd60808fd
FernandoDuffoo <fduffoo@aol.com>
2023-03-22 11:45:05 -0400

    fix

--- models/Quote.php
+++ models/Quote.php
@@ -44,16 +44,29 @@ class Quote {
 
     public function read_single() {
         // Create query
-    $query = 'SELECT 
+   
+        $query = 'SELECT 
+        a.author as author,
+        c.category as category, 
         q.id,
-        q.quote,
-        q.author_id,
-        q.category_id
-    FROM
-        ' . $this->table . ' q
-    WHERE
-        q.id = ?
-    LIMIT 1';
+        q.quote
+    FROM ' . $this->table . ' q 
+        LEFT JOIN authors as a ON q.author_id = q.id 
+        LEFT JOIN categories as c ON q.category_id = q.id
+        WHERE q.id = ?
+        LIMIT 1';
+   
+   
+   //$query = 'SELECT 
+   //     q.id,
+   //     q.quote,
+   //     q.author_id,
+   //     q.category_id
+   // FROM
+   //     ' . $this->table . ' q
+   // WHERE
+   //     q.id = ?
+   // LIMIT 1';
 
     // Prepare statement
     $stmt = $this->conn->prepare($query);
