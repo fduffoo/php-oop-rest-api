1d28e39716212239805624c424b099fb3e878531
FernandoDuffoo <fduffoo@aol.com>
2023-03-22 21:20:59 -0400

    upd

--- models/Quote.php
+++ models/Quote.php
@@ -48,8 +48,8 @@ class Quote {
     $query = 'SELECT 
         q.id,
         q.quote,
-        a.author as author,
-        c.category as category
+        a.author as author_id,
+        c.category as category_id
     FROM
         ' . $this->table . ' q
     LEFT JOIN authors as a ON q.author_id = a.id
